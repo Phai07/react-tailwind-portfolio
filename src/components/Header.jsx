d2f9ab7ac6b58a2e741a2b41527acd39bdd9eb0f
Phai <phai.339@gmail.com>
2024-12-19 23:53:13 +0700

    updatetext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,14 +34,10 @@ function Header() {
               />
             </div>
             {/* <h1 className="text-white text-5xl font-bold">Hi! I'm Phai</h1> */}
-            <p className="text-white py-4 max-w-lg text-justify">
-              Junior Software Developer, certified by Generation Bootcamp, with
-              2 years of experience as a Commis Chef. Proficient in modern web
-              development tools and basic UX/UI design. Developed strong
-              teamwork, problem-solving, and time management skills. A
-              detail-oriented and creative individual, passionate about
-              continuous learning and creating effective software solutions.
-            </p>
+            <h2 className="text-white py-4 max-w-lg text-justify text-lg">
+            It always seems impossible until it’s done.
+           </h2>
+           <p> — Nelson Mandela</p>
             <a
               className="shadow-lg bg-[#171717] rounded-md text-white text-2xl hover:mix-blend-screen py-2 px-4 inline-block"
               href={Cv}
